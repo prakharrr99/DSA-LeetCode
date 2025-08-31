30117f9eeed9477f3855ef17f668182a145e2054
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2025-08-31 23:01:04 +0530

    Time: 1 ms (80.08%) | Memory: 14.9 MB (36.12%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> ans; // nice ideaa
+        for(int i=0;i<nums.size();i++){
+            ans.push_back({nums[i],i});
+        }
+        sort(ans.begin(),ans.end());
+        int i=0; int j=nums.size()-1;
+        while(j>i){
+            if(ans[i].first+ans[j].first==target){
+                return {ans[i].second,ans[j].second};
+            }
+            else if(ans[i].first+ans[j].first>target) j--;
+            else i++;
+        }
+        return {};
+    }
+};
