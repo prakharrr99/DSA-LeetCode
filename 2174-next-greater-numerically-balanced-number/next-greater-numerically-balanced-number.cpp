f2ffd4bd6d715588fb1c14fee4f81eb6c36c2ceb
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2025-10-24 16:41:23 +0530

    Time: 98 ms (41.21%) | Memory: 9.1 MB (66.33%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -1,15 +1,22 @@
 class Solution {
 private:
     bool solve(int a){
-        map<int,int> m;
+        int arr[10]={0};
+        //map<int,int> m;
+        int c=a;
         while(a!=0){
             int b=a%10;
-            m[b]++;
+            //m[b]++;
+            arr[b]++;
             a=a/10;
         }
-
-        for(auto it:m){
-            if(it.first!=it.second) return false;
+        // for(auto it:m){
+        //     if(it.first!=it.second) return false;
+        // }
+        while(c!=0){
+            int b=c%10;
+            if(b!=arr[b]) return false;
+            c=c/10;
         }
         return true;
     }
