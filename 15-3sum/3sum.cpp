ae5dca0f8942d8ecbd421c189dcc48b84277b8bd
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2025-12-13 16:54:58 +0530

    Time: 46 ms (70.90%) | Memory: 28.9 MB (98.99%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,23 +4,19 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         for(int i=0;i<nums.size()-2;i++){
-            int p=i+1; int q=nums.size()-1;
+            int p=i+1;int q=nums.size()-1;
             if(i!=0 && nums[i]==nums[i-1]) continue;
             while(q>p){
-                if(nums[i]+nums[p]+nums[q]==0){
+                if(nums[p]+nums[q]+nums[i]==0){
                     ans.push_back({nums[i],nums[p],nums[q]});
-                    p++;
-                    q--;
-                    while(p<q && nums[p]==nums[p-1]) p++; 
-                    while(p<q && nums[q]==nums[q+1]) q--; 
-                    
+                    p++;q--;
+                    while(p<q && nums[p]==nums[p-1]) p++;
+                    while(p<q && nums[q]==nums[q+1]) q--;
                 }
-                else if(nums[i]+nums[p]+nums[q]<0) p++;
-                else q--;
+                else if(nums[p]+nums[q]+nums[i]>0) q--;
+                else p++;
             }
         }
-
         return ans;
-
     }
 };
