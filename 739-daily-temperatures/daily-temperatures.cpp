4b114ce97299efed8a525219b706a37a6e938297
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2025-12-25 14:12:11 +0530

    Time: 25 ms (43.65%) | Memory: 107.4 MB (40.80%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> s;
+        vector<int> ans(temperatures.size(),0);
+        for(int i=0;i<temperatures.size();i++){
+            while(s.size()!=0 && temperatures[i]>temperatures[s.top()]){
+                ans[s.top()]=i-s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
