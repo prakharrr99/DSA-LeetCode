924879895ab2aa39522f2d06f667c3e473589b64
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2025-12-29 17:09:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (92.93%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -5,22 +5,37 @@ public:
             digits[digits.size()-1]+=1;
             return digits;
         }
-        int c=1;
-        vector<int> ans;
-        
-        ans.push_back(0);
 
-        for(int i=digits.size()-2;i>=0;i--){
-            if(digits[i]==9){
-                int a=(digits[i]+c)%10;
-                ans.insert(ans.begin(),a);
-            }
-            else{
-                ans.insert(ans.begin(),digits[i]+c);
-                c=0;
-            }
+        vector<int> ans;
+        int i=digits.size()-1;
+        while(i>=0 && digits[i]==9){
+            ans.push_back(0);
+            digits[i]=0;
+            i--;
         }
-        if(c!=0) ans.insert(ans.begin(),1);
-        return ans;
+        if(i<0){
+             ans.insert(ans.begin(),1); return ans;
+        }
+        else{
+             digits[i]+=1; return digits;
+        }
+
+        // int c=1;
+        // vector<int> ans;
+        
+        // ans.push_back(0);
+
+        // for(int i=digits.size()-2;i>=0;i--){
+        //     if(digits[i]==9){
+        //         int a=(digits[i]+c)%10;
+        //         ans.insert(ans.begin(),a);
+        //     }
+        //     else{
+        //         ans.insert(ans.begin(),digits[i]+c);
+        //         c=0;
+        //     }
+        // }
+        // if(c!=0) ans.insert(ans.begin(),1);
+        // return ans;
     }
 };
