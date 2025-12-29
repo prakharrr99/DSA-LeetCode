fc5a5ed50a7cb345243d9e88caaa5951a965b0c5
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2025-12-29 17:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (68.98%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int a=0;
+        for(int i=1;i<=nums.size();i++){
+            a=a^i;
+        }
+        for(int i=0;i<nums.size();i++){
+            a=a^nums[i];
+        }
+        return a;
+    }
+};
