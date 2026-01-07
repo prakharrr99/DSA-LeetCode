7d1ed6a0e80abcbface695ac971fe82ab352b660
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-01-07 12:02:29 +0530

    Time: 4 ms (31.96%) | Memory: 39.2 MB (65.00%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -4,19 +4,15 @@ public:
         int a=0;
         int mini=INT_MAX;
         pair<int,int> p;
+        long long s=0;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
+                s+=abs(matrix[i][j]);
                 if(matrix[i][j]<0) a++;
                 if(abs(matrix[i][j])<mini){
                     mini=abs(matrix[i][j]);
                     p.first=i; p.second=j;
                 }
-            }
-        }
-        long long s=0;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                s+=abs(matrix[i][j]);
             }
         }
         if(a%2==0){
