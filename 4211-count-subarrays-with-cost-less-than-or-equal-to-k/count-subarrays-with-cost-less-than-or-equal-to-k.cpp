fa92f1cce617dc840ea4d8ae9df56aa723105fc0
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-02-08 19:23:34 +0530

    Time: 534 ms (14.65%) | Memory: 301 MB (7.74%) - LeetSync

--- 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
+++ 4211-count-subarrays-with-cost-less-than-or-equal-to-k/count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -6,8 +6,9 @@ public:
         int l=0;
         long long a=0;
         for(int r=0;r<nums.size();r++){
-            ms.insert(nums[r]);
-            while(r>=l && ms.size() && 1LL*(r-l+1)*(*ms.rbegin()-*ms.begin())>k){
+            ms.insert(nums[r]);  
+            // if ms will be empty and if we try to access begin and rbegin it will give runtime error so better to check ms.size()
+            while(r>=l && ms.size() && 1LL*(r-l+1)*(*ms.rbegin()-*ms.begin())>k){ 
                 ms.erase(ms.find(nums[l]));
                 l++;
             }
