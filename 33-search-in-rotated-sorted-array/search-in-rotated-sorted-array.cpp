f4c0877f0eeac743bb63f9ed0299a4bdefb0c8a7
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-02-10 20:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,36 +1,32 @@
 class Solution {
-    private:
-    int pivot(vector<int>& nums){
-        int i=0;int j=nums.size()-1;
-        int mid=i+(j-i)/2;
-        while(j>=i){
-            if(nums[mid]<nums[0]) j=mid-1;
-            else i=mid+1;
-            mid=i+(j-i)/2;
-        }
-        return mid;
-    }
 public:
     int search(vector<int>& nums, int target) {
+        int n = nums.size();
 
-        int pivotindex=pivot(nums);
-    
-        int i; int j;
-        if(target==nums[0]) return 0;
-        else if(target>nums[0]) {
-            i=1; j=pivotindex-1;
-        } 
-        else{
-            i=pivotindex;
-            j=nums.size()-1;
+        int low = 0;
+
+        int high = n-1;
+
+        while(low<=high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
+                    high = mid -1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(nums[high]>=target && nums[mid]<target){
+                    low = mid +1;
+                }
+                else{
+                    high = mid -1;
+                }
+            }
         }
-        int mid=i+(j-i)/2;
-        while(j>=i){
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]<target) i=mid+1;
-            else j=mid-1;
-            mid=i+(j-i)/2;
-        }    
         return -1;
     }
 };
