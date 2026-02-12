68b1c00dfe30d2386e586648a0192879608fce2b
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-02-12 21:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (6.56%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,10 +2,42 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            ans.push_back(nums[i]*nums[i]);
+        int j=-1;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(j==-1 && nums[i]<0) j=i+1;
+            if(nums[i]<0){
+                ans.push_back(abs(nums[i]));
+            }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+
+        if(ans.size()==0){
+            for(int i=0;i<nums.size();i++){
+                nums[i]=nums[i]*nums[i];
+            }
+            return nums;
+        }
+
+        int i=0; int k=j;
+        vector<int> a;
+        while(i<ans.size() && j<nums.size()){
+            if(ans[i]>nums[j]) a.push_back(nums[j++]);
+            else a.push_back(ans[i++]);
+        }
+        while(i<ans.size()){
+            a.push_back(ans[i++]);
+        }
+        while(j<nums.size()){
+            a.push_back(nums[j++]);
+        }
+        for(int i=0;i<a.size();i++){
+            a[i]=a[i]*a[i];
+        }
+        return a;
+
+        // for(int i=0;i<nums.size();i++){
+        //     ans.push_back(nums[i]*nums[i]);
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
     }
 };
