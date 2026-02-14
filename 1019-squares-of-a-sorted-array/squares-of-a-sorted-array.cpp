f64707b92b385df98b263e0133619b45055cd1fb
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-02-14 11:15:15 +0530

    Time: 2 ms (54.04%) | Memory: 31.2 MB (22.70%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> ans;
         int j=-1;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(j==-1 && nums[i]<0) j=i+1;
-            if(nums[i]<0){
-                ans.push_back(abs(nums[i]));
-            }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=0){ j=i; break;}
         }
-
-        if(ans.size()==0){
-            for(int i=0;i<nums.size();i++){
-                nums[i]=nums[i]*nums[i];
-            }
-            return nums;
+        
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]*nums[i];
         }
-
-        int i=0; int k=j;
-        vector<int> a;
-        while(i<ans.size() && j<nums.size()){
-            if(ans[i]>nums[j]) a.push_back(nums[j++]);
-            else a.push_back(ans[i++]);
+        int i;
+        if(j==0) return nums;
+        else if(j==-1){ 
+            j=nums.size()-1;
+            i=nums.size();
         }
-        while(i<ans.size()){
-            a.push_back(ans[i++]);
+        else{
+            i=j; j--;
         }
-        while(j<nums.size()){
-            a.push_back(nums[j++]);
+        vector<int> ans;
+        
+        while(i<nums.size() && j>=0){
+            if(nums[i]>nums[j]) ans.push_back(nums[j--]);
+            else ans.push_back(nums[i++]);
         }
-        for(int i=0;i<a.size();i++){
-            a[i]=a[i]*a[i];
+        while(i<nums.size()){
+            ans.push_back(nums[i++]);
         }
-        return a;
+        while(j>=0){
+            ans.push_back(nums[j--]);
+        }
+       
+        return ans;
 
         // for(int i=0;i<nums.size();i++){
         //     ans.push_back(nums[i]*nums[i]);
