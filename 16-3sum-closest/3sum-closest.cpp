3ff7789d4f2175453914cc4eac9d630fa52e02a4
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-02-14 18:31:49 +0530

    Time: 7 ms (87.70%) | Memory: 13.9 MB (63.45%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        int a=9999999;
-        int flag=0;
+        int a=INT_MAX; int ans=INT_MAX;
         for(int i=0;i<nums.size()-2;i++){
-            int p=i+1;
-            int q=nums.size()-1;
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            int p=i+1; int q=nums.size()-1; int f=0;
             while(q>p){
-                int s=nums[i]+nums[p]+nums[q];
-                if(s==target) return target;
-                else if(s>target){
-                    q--;
-                    if(s-target<a){
-                        a=s-target;
-                        flag=-1;
-                    }
-                } 
-                else{
+                int s=nums[i]+nums[q]+nums[p];
+                if(target==s) return s;
+                else if(target>s){
+                    if(abs(target-s)<a){ a=abs(target-s); ans=s;}
                     p++;
-                    if(target-s<a){
-                        a=target-s;
-                        flag=1;
-                    }
+                    while(q>p && nums[p]==nums[p-1]) p++;
+                }
+                else{
+                    if(abs(target-s)<a){ a=abs(target-s);  ans=s;}
+                    q--;
+                    while(q>p && nums[q]==nums[q+1]) q--;
                 }
             }
         }
-        return (flag==1)?target-a:target+a;
+        return ans;
     }
 };
