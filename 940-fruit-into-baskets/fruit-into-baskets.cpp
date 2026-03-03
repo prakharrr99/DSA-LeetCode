a9dddc462d25e2a4779c2772f07a6677a95d5b40
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-03 17:35:17 +0530

    Time: 46 ms (45.69%) | Memory: 86.8 MB (18.13%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -6,16 +6,21 @@ public:
         unordered_map<int,int> m;
         int c=0;
         while(j<fruits.size()){
+
             if(m[fruits[j]]==0) c++;
             m[fruits[j]]++;
+
+            // no.of baskets
             while(c>2){
                 if(m[fruits[i]]==1) c--;
                 m[fruits[i]]--;
                 i++;
             }
+            // for one and 2 basket 
             if(c<=2) a=max(a,j-i+1);
             j++;
         }
+        //  when freq goes from 0->1 or 1->0 then only change the count of c [CONCEPT]
         return a;
     }
 
