9641511786a18aba16d15e4bd189e14dc7e75a5e
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-10 10:51:37 +0530

    Time: 231 ms (63.41%) | Memory: 8.8 MB (71.54%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int M = 1e9 + 7;
-    int t[201][201][2];
+    int M=1e9+7;
+    int dp[201][201][2];
+    int solve(int zero,int one,bool oneused,int limit){
+        if(zero==0 && one==0) return 1;
 
-    int solve(int onesLeft, int zerosLeft, bool lastWasOne, int limit) {
-        if(onesLeft == 0 && zerosLeft == 0) {
-            return 1;
+        if(dp[zero][one][oneused]!=-1){
+            return dp[zero][one][oneused];
         }
-
-        if(t[onesLeft][zerosLeft][lastWasOne] != -1) {
-            return t[onesLeft][zerosLeft][lastWasOne];
-        }
-
-
-        int result = 0;
-
-        if(lastWasOne == true) { //explore 0s
-            for(int len = 1; len <= min(zerosLeft, limit); len++) {
-                result = (result + solve(onesLeft, zerosLeft - len, false, limit)) % M;
-            }
-        } else { //explore 1s
-            for(int len = 1; len <= min(onesLeft, limit); len++) {
-                result = (result + solve(onesLeft - len, zerosLeft, true, limit)) % M;
+        int result=0;
+        if(oneused){
+            // then go with 0
+            for(int i=1;i<=min(zero,limit);i++){
+                result=(result+solve(zero-i,one,false,limit))%M;
             }
         }
-
-        return t[onesLeft][zerosLeft][lastWasOne] = result;
-
+        else{
+            //go with 1;
+            for(int i=1;i<=min(one,limit);i++){
+                result=(result+solve(zero,one-i,true,limit))%M;
+            }
+        }
+        return dp[zero][one][oneused]=result;
     }
-
     int numberOfStableArrays(int zero, int one, int limit) {
-        memset(t, -1, sizeof(t));
-        int startWithOne = solve(one, zero, false, limit);
-        int startWithZero = solve(one, zero, true, limit);
-
-        return (startWithOne + startWithZero) % M;
-
+        memset(dp,-1,sizeof(dp));
+        int zeroes=solve(zero,one,true,limit);
+        int ones=solve(zero,one,false,limit);
+        return (zeroes+ones)%M;
     }
 };
