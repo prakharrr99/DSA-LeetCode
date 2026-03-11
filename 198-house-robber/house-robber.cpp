3746cab23c33140951cd66099ed23b29712ed1f0
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-11 19:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (61.14%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int id,vector<int>& nums,vector<int>& dp){
+        if(id==nums.size()-1 ){
+            return nums[id];
+        }
+        if(id>=nums.size()) return 0;
+        
+        if(dp[id]!=-1) return dp[id];
+
+        int pick=solve(id+2,nums,dp)+nums[id];
+
+        int notpick=solve(id+1,nums,dp);
+
+        return dp[id]=max(pick,notpick);
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,-1);
+        return solve(0,nums,dp);
+    }
+};
