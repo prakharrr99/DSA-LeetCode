ab97537a4bb2db5c9152333a53a50f2903a85f2a
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-12 19:31:40 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (97.92%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int s=0; int maxi=INT_MIN; 
-        int start=-1; int end=-1; // IF WE WANT TO PRINT THE SUB-ARRAY
-        for(int i=0;i<nums.size();i++){
-            if(s==0) start=i;
-            s+=nums[i];
-            if(maxi<s){
-                maxi=s;
-                end=i;
-            }
-            if(s<0) s=0;
+        // int s=0; int maxi=INT_MIN; 
+        // int start=-1; int end=-1; // IF WE WANT TO PRINT THE SUB-ARRAY
+        // for(int i=0;i<nums.size();i++){
+        //     if(s==0) start=i;
+        //     s+=nums[i];
+        //     if(maxi<s){
+        //         maxi=s;
+        //         end=i;
+        //     }
+        //     if(s<0) s=0;
+        // }
+        // return maxi;
+        //KADANE's ALGO
+
+        //PRATYUSH NARAIN
+
+        int ans=nums[0];
+        int best_ending=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int v1=nums[i];
+            int v2=best_ending+nums[i];
+            best_ending=max(v1,v2);
+            ans=max(ans,best_ending);
         }
-        return maxi;
+        ans=max(ans,best_ending);
+        // if(ans<0) ans=0;
+        return ans;
     }
 };
