1752f5fea92d5fa9c5d8080045a45035ed5453d1
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-14 10:59:51 +0530

    Time: 23 ms (26.85%) | Memory: 25.3 MB (28.09%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -6,7 +6,7 @@ public:
             return;
         }
         for(int i=0;i<s.size();i++){
-            if(a.size()==0 || a.back()!=s[i]){
+            if(a.size()==0 || a.back()!=s[i]){ // without this it will take more time
                 a.push_back(s[i]);
                 solve(n,a,ans,s);
                 // this is not about subsequences it should choose any character at a particular call until the size becomes n
@@ -19,18 +19,19 @@ public:
         string a;
         solve(n,a,ans,"abc");
 
-        sort(ans.begin(),ans.end());
-        vector<string> p;
-        for(int i=0;i<ans.size();i++){
-            int b=0;
-            for(int j=0;j<n-1;j++){
-                if(ans[i][j]==ans[i][j+1]){
-                    b=1; break;
-                }
-            }
-            if(b==0) p.push_back(ans[i]);
-        }
-        if(k>p.size()) return "";
-        return p[k-1];
+        // sort(ans.begin(),ans.end());
+        // vector<string> p;
+        // for(int i=0;i<ans.size();i++){
+        //     int b=0;
+        //     for(int j=0;j<n-1;j++){
+        //         if(ans[i][j]==ans[i][j+1]){
+        //             b=1; break;
+        //         }
+        //     }
+        //     if(b==0) p.push_back(ans[i]);
+        // }                                    // this is for without check
+
+        if(k>ans.size()) return "";
+        return ans[k-1];
     }
 };
