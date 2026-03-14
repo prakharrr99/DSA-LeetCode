66e51f063940416efb9d3341e539e824533ba806
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-14 17:29:17 +0530

    Time: 27 ms (40.01%) | Memory: 88.1 MB (92.39%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        vector<int> p;
-        int j=0;
-        int s=0; int ans=0;
-        while(j<nums.size()){
-            if(nums[j]==0) s+=(-1);
-            else s+=1;
-            p.push_back(s);
-            j++;
-        }
+        int zero=0; int one=0;
+        int ans=0;
         unordered_map<int,int> m;
         for(int i=0;i<nums.size();i++){
-            if(p[i]==0) ans=max(ans,i+1);
-            if(m[p[i]]==0){
-                if(i==0) m[p[i]]=INT_MAX;
-                else m[p[i]]=i;
-            }
-            else{
-                if(m[p[i]]==INT_MAX) ans=max(i-0,ans);
-                else ans=max(i-m[p[i]],ans);
-            }
+            if(nums[i]==0) zero++; // this is for counting
+            else one++;
+
+            if(one-zero==0) ans=max(ans,i+1); //edge case
+
+            if(m.find(one-zero)!=m.end()) ans=max(ans,i-m[one-zero]);  // if found update
+            else m[one-zero]=i;     // else map with the firt index where that diff occur as we have to find the longest subarray 
+            // we dont have to count
+
         }
         return ans;
+
+        // find the condition
+        // make green part satisfy the condition 
+        // then the red part which is left will have the same output(here diff) as the whole subarray
+        // map the red part if it exist then green part will also exist count it , form the longest or do whatever is required
+
     }
 };
