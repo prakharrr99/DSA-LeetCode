3eed30207278b1b6b59da3081947d931b4daab0b
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-17 23:03:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.03%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> ss;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='{' || s[i]=='[' || s[i]=='('){
-                ss.push(s[i]);
-            }
-            else if(ss.empty()){
-                return false;
-            }
+    bool isValid(string str) {
+        stack<char> s;
+        for(int i=0;i<str.size();i++){
+            if(str[i]=='{' || str[i]=='[' || str[i]=='(') s.push(str[i]);
             else{
-                int a=0;
-                if((ss.top()=='[' && s[i]==']') || (ss.top()=='{' && s[i]=='}') || (ss.top()=='(' && s[i]==')')){
-                    ss.pop();
-                    a=1;
-                }
-                if(a==0) return false;
+                if(s.size()==0) return false;
+                if((str[i]=='}' && s.top()=='{') || (str[i]==']' && s.top()=='[') || (str[i]==')' && s.top()=='(')) 
+                    s.pop();
+                else return false;
             }
         }
-        if(ss.empty()) return true;
+        if(s.size()==0) return true;
         return false;
     }
 };
