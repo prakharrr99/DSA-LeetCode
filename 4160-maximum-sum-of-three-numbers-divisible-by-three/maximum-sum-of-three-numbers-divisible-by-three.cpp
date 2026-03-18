16d128b2ddfbc463980d9d490c8597f37a0b5daf
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-18 20:12:29 +0530

    Time: 96 ms (51.71%) | Memory: 289.8 MB (62.67%) - LeetSync

--- 4160-maximum-sum-of-three-numbers-divisible-by-three/maximum-sum-of-three-numbers-divisible-by-three.cpp
+++ 4160-maximum-sum-of-three-numbers-divisible-by-three/maximum-sum-of-three-numbers-divisible-by-three.cpp
@@ -1,34 +1,21 @@
 class Solution {
-private:
-    int sum(vector<int>& a){
-        if(a.size()<3) return 0;
-        return a[a.size()-1]+a[a.size()-2]+a[a.size()-3];
-        // int m=INT_MIN;
-        // for(int i=a.size()-1;i>=0;i--){
-        //     if(m!=INT_MIN) return m;
-        //     int j=i-1;
-        //     int k=i-2;
-        //     if(j<0 || k<0) return m;
-        //     m=a[i]+a[j]+a[k];
-        // }
-        // return m;
-    }
 public:
     int maximumSum(vector<int>& nums) {
+        vector<int> r0;
+        vector<int> r1;
+        vector<int> r2;
         sort(nums.begin(),nums.end());
-        vector<int> z;
-        vector<int> o;
-        vector<int> t;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]%3==0) z.push_back(nums[i]);
-            else if(nums[i]%3==1) o.push_back(nums[i]);
-            else t.push_back(nums[i]);
+            if(nums[i]%3==0) r0.push_back(nums[i]);
+            else if(nums[i]%3==1) r1.push_back(nums[i]);
+            else r2.push_back(nums[i]);
         }
-        int s=INT_MIN;
-        s=max(sum(z),s);
-        s=max(sum(o),s);
-        s=max(sum(t),s);
-        if(z.size() && o.size() && t.size()) s=max(s,z[z.size()-1]+o[o.size()-1]+t[t.size()-1]);
-        return s;
+        int ans=0;
+        if(r1.size()>2) ans=max(r1[r1.size()-1]+r1[r1.size()-2]+r1[r1.size()-3],ans);
+        if(r2.size()>2) ans=max(r2[r2.size()-1]+r2[r2.size()-2]+r2[r2.size()-3],ans);
+        if(r0.size()>2) ans=max(r0[r0.size()-1]+r0[r0.size()-2]+r0[r0.size()-3],ans);
+
+        if(r1.size()!=0 && r2.size()!=0 && r0.size()!=0) ans=max(ans,r0[r0.size()-1]+r1[r1.size()-1]+r2[r2.size()-1]);
+        return ans;
     }
 };
