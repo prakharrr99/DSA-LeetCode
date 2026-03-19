2493b031542eab85ec7caf9812bc2181769b558d
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-19 19:37:52 +0530

    Time: 22 ms (69.57%) | Memory: 88.2 MB (92.25%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -4,11 +4,12 @@ public:
         int zero=0; int one=0;
         int ans=0;
         unordered_map<int,int> m;
+        m[0]=-1;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0) zero++; // this is for counting
             else one++;
 
-            if(one-zero==0) ans=max(ans,i+1); //edge case
+            // if(one-zero==0) ans=max(ans,i+1); //edge case
 
             if(m.find(one-zero)!=m.end()) ans=max(ans,i-m[one-zero]);  // if found update
             else m[one-zero]=i;     // else map with the firt index where that diff occur as we have to find the longest subarray 
