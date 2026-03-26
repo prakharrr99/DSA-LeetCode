64b117f5970e54b4635d7e44d0cc8d903c8933c6
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-26 11:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (29.29%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -10,8 +10,25 @@ public:
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
+        
         vector<vector<int>> dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),-1));
 
-        return solve(obstacleGrid,obstacleGrid.size()-1,obstacleGrid[0].size()-1,dp);
+        // MEMOIZATION
+        // return solve(obstacleGrid,obstacleGrid.size()-1,obstacleGrid[0].size()-1,dp);
+
+        // TABULATION
+        for(int i=0;i<dp.size();i++){
+            for(int j=0;j<dp[0].size();j++){
+                if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0,l=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) l=dp[i][j-1];
+                    dp[i][j]=up+l;
+                }
+            }
+        }
+        return dp[dp.size()-1][dp[0].size()-1];
     }
 };
