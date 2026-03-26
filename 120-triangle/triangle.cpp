97e5f7b28a9ff9b7c727f9d0120f5ae95c8ea9fa
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-26 20:28:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (8.29%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>& dp){
+        // if(i>=triangle.size() || j>= triangle[i].size()) return INT_MAX; // not required because we will not go out of boundary
+        if(i==triangle.size()-1) return triangle[i][j];
+
+        if(dp[i][j]!=-1) return dp[i][j];
+        int same_col=triangle[i][j]+solve(triangle,i+1,j,dp);
+        int next_col=triangle[i][j]+solve(triangle,i+1,j+1,dp);
+
+        return min(same_col,next_col);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),-1));
+
+        // return solve(triangle,0,0,dp);
+
+        for(int j=0;j<triangle[triangle.size()-1].size();j++){
+            dp[triangle.size()-1][j]=triangle[triangle.size()-1][j];
+        }
+
+        for(int i=triangle.size()-2;i>=0;i--){ // is set the base case for n-1 thats why started from n-2
+            for(int j=triangle[i].size()-1;j>=0;j--){
+                int same_col=triangle[i][j]+dp[i+1][j];
+                int next_col=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(same_col,next_col);
+            }
+        }
+        return dp[0][0]; 
+    }
+};
