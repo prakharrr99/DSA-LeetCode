94b275792ac00f47ec7a99906d2ddd93f15ec4d4
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-03-30 17:25:07 +0530

    Time: 2352 ms (5.17%) | Memory: 19.2 MB (90.23%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         for(int i=0;i<s1.size();i++){
-            if(s1[i]==s2[i]) continue;
-            else{
+            if(s1[i]!=s2[i]){
                 for(int j=i+2;j<s1.size();j+=2){
                     if(s2[i]==s1[j]){
                         swap(s1[j],s1[i]);
@@ -13,7 +12,6 @@ public:
                 if(s1[i]!=s2[i]) return false;
             }
         }
-        if(s1==s2) return true;
-        return false;
+        return s1==s2;
     }
 };
