a6d91303cd018bd4f876037add0cee71f5f83ef2
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-05-11 18:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
+    int min_in_rotated_sorted(vector<int>& nums){
+        if(nums.size()==1) return 0;
+        if(nums[0]<nums[nums.size()-1]) return 0;
+        int ans=-1;
 
-        int low = 0;
-
-        int high = n-1;
-
-        while(low<=high){
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && nums[mid]>target){
-                    high = mid -1;
-                }else{
-                    low = mid + 1;
-                }
-            }else{
-                if(nums[high]>=target && nums[mid]<target){
-                    low = mid +1;
-                }
-                else{
-                    high = mid -1;
-                }
+        int low=0;
+        int high=nums.size()-1;
+        while(high>=low){
+            int mid=low+(high-low)/2;
+            if(nums[0]<=nums[mid]){
+                low=mid+1;
             }
+            else{
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+    int search(vector<int>& nums, int target) {
+        if(nums.size()==1){
+            if(nums[0]==target) return 0;
+            return -1;
+        }
+        int low=0; int high=0;
+        if(nums[0]<=target){
+            low=0;
+            high=min_in_rotated_sorted(nums)-1;
+        }
+        else{
+            low=min_in_rotated_sorted(nums);
+            high=nums.size()-1;
+        }
+        
+        if(nums[0]<nums[nums.size()-1]){ low=0; high=nums.size()-1;}// WHEN ARRAY IS ALREADY SORTED
+
+        while(high>=low){
+            int mid=low+(high-low)/2;
+            if(target==nums[mid]) return mid;
+            else if(target>nums[mid]) low=mid+1;
+            else high=mid-1;
         }
         return -1;
     }
