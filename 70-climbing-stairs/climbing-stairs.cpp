6ec7050542917258903a34a4dc210405502e238e
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-05-13 08:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.52%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    //MEMOIZATION
     int solve(int n,vector<int>& dp){
-        if(n==0 || n==1) return 1;
+        if(n<=2) return n;
         if(dp[n]!=-1) return dp[n];
         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
     }
-
-    // TABULATION
-    int solve1(int n,vector<int>& dp){
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-    
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return solve1(n,dp);
+        return solve(n,dp);
     }
 };
