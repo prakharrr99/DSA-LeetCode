ff87c30cc7687535be9d1ed135ab413a9c8eda55
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-05-19 07:38:26 +0530

    Time: 187 ms (14.05%) | Memory: 204.1 MB (7.71%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int n,int s,vector<vector<int>>& dp){
-        if(s==0) return 1;
-        if(n==0){
-            if(s-nums[0]==0) return 1;
+    int solve(vector<int>& nums,int i,int target,vector<vector<int>>& dp){
+        if(target==0) return 1;
+        if(target<0) return 0;
+        if(i==0){
+            if(target==nums[i]) return 1;
             return 0;
         }
-        if(s<0) return 0;
-
-        if(dp[n][s]!=-1) return dp[n][s];
-        if(solve(nums,n-1,s-nums[n],dp)+solve(nums,n-1,s,dp)>=2) return dp[n][s]=2;
-        return dp[n][s]=solve(nums,n-1,s-nums[n],dp)+solve(nums,n-1,s,dp);
+        if(dp[i][target]!=-1) return dp[i][target];
+        if(solve(nums,i-1,target-nums[i],dp)+solve(nums,i-1,target,dp)>=2) return dp[i][target]=2;
+        return dp[i][target]=solve(nums,i-1,target-nums[i],dp)+solve(nums,i-1,target,dp);
     }
     bool canPartition(vector<int>& nums) {
+
         int s=0;
         for(int i=0;i<nums.size();i++) s+=nums[i];
         if(s&1) return false;
-        vector<vector<int>> dp(nums.size(),vector<int>(s,-1));
-        s=s/2;
-        return 2<=solve(nums,nums.size()-1,s,dp);
-    }
 
+        vector<vector<int>> dp(nums.size(),vector<int>(s,-1));
+
+        return solve(nums,nums.size()-1,s/2,dp)>=2;
+
+    }
 };
