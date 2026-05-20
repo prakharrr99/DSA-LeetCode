c1c41c2a43d8a2700a7dfca6d990de199c54f133
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-05-20 07:42:17 +0530

    Time: 11 ms (41.13%) | Memory: 88.5 MB (23.72%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -15,5 +15,18 @@ public:
             ans.push_back(c);
         }
         return ans;
+
+        // int n=A.size();
+        // vector<int>res(n+1,0);
+        // vector<int>ress;
+        // int a=0;
+        // for(int i=0;i<n;i++) {
+        //     if(++res[A[i]]==2)
+        //     a++;
+        //     if(++res[B[i]]==2)
+        //     a++;
+        //     ress.push_back(a);
+        // }
+        // return ress;
     }
 };
