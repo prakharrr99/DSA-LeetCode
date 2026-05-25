b8dc52f376faab8e843f7870c04267c5d982f330
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-05-25 11:57:11 +0530

    Time: 1 ms (14.29%) | Memory: 87.1 MB (74.84%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        unordered_map<int,int> m;
+        int c5=0;
+        int c10=0;
+      
         for(int i=0;i<bills.size();i++){
-            if(bills[i]==5) m[5]++;
+            if(bills[i]==5) c5++;
             else if(bills[i]==10){
-                if(m[5]>0){
-                    m[5]--;
-                    m[10]++;
+                if(c5>0){
+                    c5--;
+                    c10++;
                 }
                 else return false;
             }
             else{
-                if(m[10]>0 && m[5]>0){
-                    m[10]--;
-                    m[5]--;
+                if(c10>0 && c5>0){
+                    c10--;
+                    c5--;
                 }
-                else if(m[5]>0){
-                    if(m[5]<3) return false;
-                    else m[5]=m[5]-3;
-                }
-                else{
-                    return false;
-                }
+                else if(c5>2) c5-=3;
+                else return false;
             }
         }
         return true;
