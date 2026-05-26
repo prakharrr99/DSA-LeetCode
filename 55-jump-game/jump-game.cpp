767589bf591a81e0e58fec0e41b2a0804d120333
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-05-26 09:05:09 +0530

    Time: 120 ms (14.82%) | Memory: 55 MB (7.73%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -13,7 +13,23 @@ public:
     bool canJump(vector<int>& nums) {
         if(nums.size()==1) return true;
         if(nums[0]==0) return false;
+
         vector<int> dp(nums.size(),-1);
+
+        dp[nums.size()-1]=1;
+        for(int i=0;i<nums.size();i++) if(nums[i]==0) dp[i]=0;
+
+        for(int i=nums.size()-2;i>=0;i--){
+            int a=0;
+            for(int j=1;j<=nums[i];j++){
+                if(i+j>=nums.size()-1 || dp[i+j]){
+                    a=1;
+                    break;
+                }
+            }
+            dp[i]=a;
+        }
+        return dp[0];
         return solve(nums,0,dp);
     }
 };
