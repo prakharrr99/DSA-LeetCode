6bdf8865b72db65235b5cbcb4e2da8cc39a78f14
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-05-28 11:38:56 +0530

    Time: 66 ms (8.71%) | Memory: 48.9 MB (35.73%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -9,7 +9,7 @@ public:
             return 1e5;
         }
         if(dp[i][amount]!=-1) return dp[i][amount];
-        
+
         int take_same=1e5;
         int take_diff=1e5;
         if(amount-coins[i]>=0) take_same=solve(coins,amount-coins[i],i,dp)+1;
@@ -20,6 +20,28 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+
+        for(int i=0;i<coins.size();i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=amount;i++){
+            if(i<coins[0]) dp[0][i]=1e5;
+            else if(i%coins[0]==0) dp[0][i]=i/coins[0];
+            else dp[0][i]=1e5;
+        }
+        for(int i=1;i<coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                int take_same=1e5;
+                int take_diff=1e5;
+                if(j-coins[i]>=0) take_same=dp[i][j-coins[i]]+1;
+                if(j-coins[i]>=0) take_diff=dp[i-1][j-coins[i]]+1;
+                int no_take=dp[i-1][j];
+
+                dp[i][j]=min(take_same,min(take_diff,no_take));
+            }
+        }
+        if(dp[coins.size()-1][amount]==1e5) return -1;
+        return dp[coins.size()-1][amount];
 
         int a=solve(coins,amount,coins.size()-1,dp);
         if(a==1e5) return -1;
