98c5310c6b761cc043915ecb92dc311f14fd7e3e
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-06-04 21:26:15 +0530

    Time: 205 ms (5.13%) | Memory: 385.6 MB (7.61%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(string s1,string s2,int i,int j,vector<vector<int>>& dp){
+    int solve(string& s1,string& s2,int i,int j,vector<vector<int>>& dp){
 
         // if(i<0 || j<0) return 0;
         // WE CAN WRITE TABULATION FOR THIS 
@@ -21,18 +21,18 @@ public:
         reverse(s.begin(),s.end());
         int n=s1.size();
         vector<vector<int>> dp(1003,vector<int>(1003,-1));
-        
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s1[i-1]==s[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
-            }
-        }
-        return dp[n][n];
+
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0]=0;
+        //     dp[0][i]=0;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         if(s1[i-1]==s[j-1]) dp[i][j]=1+dp[i-1][j-1];
+        //         else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+        //     }
+        // }
+        // return dp[n][n];
 
 
         return solve(s1,s,n,n,dp);
