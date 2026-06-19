188563fcb0e83da9a485d18f9219d700ffcf4fdd
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-06-19 17:42:32 +0530

    Time: 2 ms (34.75%) | Memory: 7.9 MB (14.85%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
-        // vector<unsigned long long> store;
-        unsigned long long ans=1;
-        // store.push_back(1);
-        if(ans%k==0) return 1;
-
-        int i=0;
-        while(i<=1e7){
+       
+        if(k%2==0 || k%5==0) return -1;
+        unsigned long long ans=1%k;
+        int i=1;
+        while(ans!=0){
             ans=(ans*10+1)%k;
-            // store.push_back(ans);
-            if(ans==0) return i+2;
             i++;
         }
-        return -1;
-
-        // int id=-1;
-        // for(int i=0;i<store.size();i++){
-        //     if(store[i]%k==0){
-        //         id=i;
-        //         break;
-        //     }
-        // }
-        // if(id==-1) return -1;
-        // int len=0;
-        // unsigned long long m=store[id];
-        // while(m){
-        //     len++;
-        //     m=m/10;
-        // }
-        // return len;
+        return i;
     }
 };
