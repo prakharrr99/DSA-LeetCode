f52d8744231fbf9d2cdf7e62266a934c3eed27d1
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-06-21 11:26:41 +0530

    Time: 277 ms (5.09%) | Memory: 22 MB (99.76%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,19 +4,19 @@ public:
         vector<vector<string>> r;
 
         int n=strs.size();
-        vector<string> temp;
+        vector<string> str=strs;
+        for(int i=0;i<n;i++){
+            sort(str[i].begin(),str[i].end());
+        }
         vector<int> v(n,0);
+        vector<string> temp;
         for(int i=0;i<n;i++){
             if(v[i]==1) continue;
-            string s=strs[i];
-            temp.push_back(s);
-            sort(s.begin(),s.end());
             v[i]=1;
+            temp.push_back(strs[i]);
             for(int j=i+1;j<n;j++){
-                if(v[j]==1 || s.size()!=strs[j].size()) continue;
-                string s1=strs[j];
-                sort(s1.begin(),s1.end());
-                if(s1==s){
+                if(v[j]==1 || str[i].size()!=str[j].size()) continue;
+                if(str[i]==str[j]){
                     temp.push_back(strs[j]);
                     v[j]=1;
                 }
