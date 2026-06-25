2e8856cb2464858010ec7d5416fc1c7d1baf91b4
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-06-25 09:49:55 +0530

    Time: 154 ms (6.95%) | Memory: 54.6 MB (21.17%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
     bool f(string& s,string& p,int i,int j,vector<vector<int>>& dp){
-        if(i<0 && j<0) return true;
+        // SEE PREVIOUS SUBMITTED CODE FOR ORIGINAL 
+        // THIS IS SHIFT INDEXING
 
-        if(i<0 && j>=0){
-            for(int a=0;a<=j;a++){
-                if(p[a]!='*') return false;
+        if(i==0 && j==0) return true;
+
+        if(i==0 && j>=1){
+            for(int a=1;a<=j;a++){
+                if(p[a-1]!='*') return false;
             }
             return true;
         }
 
-        if(j<0 && i>=0) return false;
+        if(j==0 && i>=1) return false;
 
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] || p[j]=='?') {
+        if(s[i-1]==p[j-1] || p[j-1]=='?') {
             return dp[i][j]=f(s,p,i-1,j-1,dp);
         }
-        if(p[j]=='*'){
+        if(p[j-1]=='*'){
             return dp[i][j]=f(s,p,i-1,j,dp)|f(s,p,i,j-1,dp);
         }
         return false;
     }
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size(),vector<int>(p.size(),-1));
-        return f(s,p,s.size()-1,p.size()-1,dp);
+        vector<vector<int>> dp(s.size()+1,vector<int>(p.size()+1,-1));
+
+        dp[0][0]=1;
+        int b=0;
+        for(int i=1;i<p.size();i++){
+            if(p[i]=='*') dp[0][i]=1;
+            else{
+                b=1;
+            }
+            if(b==1) dp[0][i]=0;
+        }
+        for(int i=1;i<s.size();i++){
+            dp[i][0]=false;
+        }
+
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=p.size();j++){
+                int one=0;
+                int two=0;
+                if(s[i-1]==p[j-1] || p[j-1]=='?') {
+                    one=f(s,p,i-1,j-1,dp);
+                }
+                if(p[j-1]=='*'){
+                   two=f(s,p,i-1,j,dp)|f(s,p,i,j-1,dp);
+                }
+                dp[i][j]=one|two;
+            }
+        }
+        dp[s.size()][p.size()];
+        return f(s,p,s.size(),p.size(),dp);
     }
 };
