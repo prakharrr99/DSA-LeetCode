7fa321f3ac71d4af2d9ba19a80a73d2fab03b6f9
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-06-26 16:53:06 +0530

    Time: 403 ms (43.11%) | Memory: 211.5 MB (44.61%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -15,7 +15,25 @@ public:
         return 0;
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(3,-1)));
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,-1)));
+
+        for(int i=0;i<prices.size();i++){
+            dp[i][1][2]=0;
+            dp[i][0][2]=0;
+        }
+        dp[prices.size()][1][0]=dp[prices.size()][1][1]=dp[prices.size()][1][2]=0;
+        dp[prices.size()][0][0]=dp[prices.size()][0][1]=dp[prices.size()][0][2]=0;
+
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+                for(int k=1;k>=0;k--){
+
+                    if(j==1) dp[i][j][k]=max(-prices[i]+dp[i+1][0][k],0+dp[i+1][1][k]);
+                    else dp[i][j][k]=max(prices[i]+dp[i+1][1][k+1],0+dp[i+1][0][k]);
+                }
+            }
+        }
+        return dp[0][1][0];
         return f(prices,0,1,0,dp);
     }
 };
