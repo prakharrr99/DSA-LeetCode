02544aa75afd308fbb0bd0ca677cb7bc54c2cf0a
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-06-27 15:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (62.61%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -10,9 +10,23 @@ public:
         return dp[i][buy]=max(prices[i]+f(prices,i+2,1,dp),0+f(prices,i+1,0,dp));
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
+        vector<vector<int>> dp(prices.size()+2,vector<int>(2,0));
 
-        
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j==1){
+                   dp[i][j]=max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                }
+                else dp[i][j]=max(prices[i]+dp[i+2][1],0+dp[i+1][0]);
+            }
+        }
+        return dp[0][1];
+
+
+        // RECURSION AND MEMOIZATION
+
+        // vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
         return f(prices,0,1,dp);
+        //jo isme call marte hai wahi tabulation and space optimization mai return marte hai !!!
     }
 };
