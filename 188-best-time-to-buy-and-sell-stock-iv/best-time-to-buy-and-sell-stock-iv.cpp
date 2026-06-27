40549a7454c2cd3ee487c34c84ddb5ec80f01845
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-06-27 15:24:03 +0530

    Time: 11 ms (43.00%) | Memory: 18.2 MB (48.90%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -15,25 +15,21 @@ public:
         return 0;
     }
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        //FOR RECURSION AND MEMOIZATION
+        // vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
 
-        // for(int i=0;i<prices.size();i++){
-        //     dp[i][1][2]=0;
-        //     dp[i][0][2]=0;
-        // }
-        // dp[prices.size()][1][0]=dp[prices.size()][1][1]=dp[prices.size()][1][2]=0;
-        // dp[prices.size()][0][0]=dp[prices.size()][0][1]=dp[prices.size()][0][2]=0;
 
-        // for(int i=prices.size()-1;i>=0;i--){
-        //     for(int j=1;j>=0;j--){
-        //         for(int k=1;k>=0;k--){
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>>(2,vector<int>(k+1,0)));
 
-        //             if(j==1) dp[i][j][k]=max(-prices[i]+dp[i+1][0][k],0+dp[i+1][1][k]);
-        //             else dp[i][j][k]=max(prices[i]+dp[i+1][1][k+1],0+dp[i+1][0][k]);
-        //         }
-        //     }
-        // }
-        // return dp[0][1][0];
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int l=k-1;l>=0;l--){
+                    if(j==1) dp[i][j][l]=max(-prices[i]+dp[i+1][0][l],0+dp[i+1][1][l]);
+                    else dp[i][j][l]=max(prices[i]+dp[i+1][1][l+1],0+dp[i+1][0][l]);
+                }
+            }
+        }
+        return dp[0][1][0];
         return f(prices,0,1,0,k,dp);
     }
 };
