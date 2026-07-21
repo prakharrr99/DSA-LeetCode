5b6c621864a00a81e2371313a27b1ceaca922692
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-07-21 19:26:50 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (21.07%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -10,19 +10,17 @@
  * };
  */
 class Solution {
-private:
+public:
     int height(TreeNode* root){
         if(root==NULL) return 0;
-        return 1+max(height(root->left),height(root->right));
+        return max(height(root->left)+1,height(root->right)+1);
     }
-public:
     bool isBalanced(TreeNode* root) {
         if(root==NULL) return true;
         int l=height(root->left);
         int r=height(root->right);
         if(abs(l-r)>1) return false;
-        if(!isBalanced(root->left)) return false;
-        if(!isBalanced(root->right)) return false;
-        return true;
+
+        return isBalanced(root->left) & isBalanced(root->right);
     }
 };
