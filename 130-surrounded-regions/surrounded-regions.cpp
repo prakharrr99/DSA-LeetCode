093678cae39dd9f8e30be2fe4b3d678df055fdee
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-07-30 18:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (24.64%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board,vector<vector<int>>& v,int i,int j){
-        v[i][j]=1;
-        int m=board.size();
-        int n=board[0].size();
-        if(i>0 && board[i-1][j]=='O' && !v[i-1][j]){
-            dfs(board,v,i-1,j);
-        }
-        if(i<m-1 && board[i+1][j]=='O' && !v[i+1][j]){
-            dfs(board,v,i+1,j);
-        }
-        if(j>0 && board[i][j-1]=='O' && !v[i][j-1]){
-            dfs(board,v,i,j-1);
-        }
-        if(j<n-1 && board[i][j+1]=='O' && !v[i][j+1]){
-            dfs(board,v,i,j+1);
-        }
-    }
     void solve(vector<vector<char>>& board) {
         int m=board.size();
         int n=board[0].size();
+
         vector<vector<int>> v(m,vector<int>(n,0));
-
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]=='O' && (i==0 || i==m-1 || j==0 || j==n-1)){
-                    dfs(board,v,i,j);
+                if((i==0 || i==(m-1) || j==0 || j==(n-1)) && board[i][j]=='O'){
+                    q.push({i,j});
+                    v[i][j]=1;
                 }
+            }
+        }
+
+        while(!q.empty()){
+            pair<int,int> p=q.front();
+            q.pop();
+            int r=p.first;
+            int c=p.second;
+            if(r-1>=0 && v[r-1][c]==0 && board[r-1][c]=='O'){
+                v[r-1][c]=1;
+                q.push({r-1,c});
+            }
+            if(c-1>=0 && v[r][c-1]==0 && board[r][c-1]=='O'){
+                v[r][c-1]=1;
+                q.push({r,c-1});
+            }
+            if(r+1<m && v[r+1][c]==0 && board[r+1][c]=='O'){
+                v[r+1][c]=1;
+                q.push({r+1,c});
+            }
+            if(c+1<n && v[r][c+1]==0 && board[r][c+1]=='O'){
+                v[r][c+1]=1;
+                q.push({r,c+1});
             }
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(v[i][j]==0){
-                    board[i][j]='X';
-                }
+                if(board[i][j]=='O' && v[i][j]==0) board[i][j]='X';
             }
         }
+
     }
 };
