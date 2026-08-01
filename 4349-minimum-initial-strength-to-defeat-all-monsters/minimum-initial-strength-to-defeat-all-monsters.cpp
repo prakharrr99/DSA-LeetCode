639ce80412dc56ed01b364a16689e2cc1e3ad5d3
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-08-02 01:05:42 +0530

    Time: 129 ms (10.00%) | Memory: 243.4 MB (40.00%) - LeetSync

---
+++ 4349-minimum-initial-strength-to-defeat-all-monsters/minimum-initial-strength-to-defeat-all-monsters.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool ispossible(long long mid,vector<int>& mon,vector<long long>& diff){
+        for(int idx=0;idx<mon.size();idx++){
+            
+            if(mid+diff[idx]<mon[idx]) return false;
+            mid=mid-mon[idx];
+            if(mid<0) mid=0;
+        }
+        return true;
+    }
+    long long minInitialStrength(vector<int>& monsters, vector<vector<int>>& boosts) {
+        sort(boosts.begin(),boosts.end());
+        
+        vector<long long> diff(monsters.size()+1,0);
+
+        for(int i=0;i<boosts.size();i++){
+            int l=boosts[i][0];
+            int r=boosts[i][1];
+
+            diff[l]+=boosts[i][2];
+            if(r+1<diff.size()) diff[r+1]-=boosts[i][2];
+        }
+
+        for(int i=1;i<diff.size();i++){
+            diff[i]+=diff[i-1];
+        }
+        
+        long long low=0;
+        long long high=0;
+        for(auto it:monsters){
+            high+=it;
+        }
+
+        long long ans=high;
+        while(high>=low){
+            long long mid=low+(high-low)/2;
+            if(ispossible(mid,monsters,diff)){
+                ans=min(ans,mid);
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+    
+};
