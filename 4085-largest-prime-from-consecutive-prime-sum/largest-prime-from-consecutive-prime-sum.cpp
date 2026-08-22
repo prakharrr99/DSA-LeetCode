dba634592566829eb26c67574fe95960adec7084
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-08-22 18:41:41 +0530

    Time: 326 ms (70.15%) | Memory: 11.1 MB (71.04%) - LeetSync

--- 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.cpp
+++ 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
+    bool prime(int n){
+        for(int i=2;i<n;i++){
+            if(n%i==0) return false;
+        }
+        if(n==1) return false;
+        return true;
+    }
     int largestPrime(int n) {
-
-        vector<bool> prime(n+8,1);
-        prime[0]=prime[1]=false;
+        vector<int> arr; int a=0;
         for(int i=2;i<=n;i++){
-            if(prime[i]){
-                for(int j=i*2;j<=n;j+=i){
-                    prime[j]=0;
-                }
+            if(prime(i)){
+                if(a==0) arr.push_back(i);
+                else{
+                    int aa=arr[a-1]+i;
+                    if(aa>n) break;
+                    arr.push_back(aa);
+                } 
+                a++;
             }
         }
-
+        if(arr.size()==0) return 0;
         int ans=0;
-        long long s=0;
-        for(int i=2;i<=n;i++){
-            if(prime[i]){
-                s+=i;
-                if(s>n) break;
-                if(prime[s]) ans=s;
+        for(int i=arr.size()-1;i>=0;i--){
+            if(prime(arr[i])){
+                return arr[i];
             }
         }
-        return ans;
+        return 0;
     }
 };
