f1cec6c6c4f90f2e6b6b3fb6938f9617474af604
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-08-26 13:15:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (82.00%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj,vector<int>& v,int node){
-        v[node]=1;
-        for(auto it:adj[node]){
-            if(v[it]==0) dfs(adj,v,it);
-        }
-    }
-    int makeConnected(int n, vector<vector<int>>& c) {
-        vector<vector<int>> adj(n);
-
-        if(n-1>c.size()) return -1;
-
-        for(int i=0;i<c.size();i++){
-            int u=c[i][0];
-            int v=c[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        int comp=0;
-        vector<int> v(n,0);
-        for(int i=0;i<n;i++){
-            if(v[i]==0){
-                 dfs(adj,v,i);
-                 comp++;
+    class DSU{
+        vector<int> parent,size;
+        public:
+        DSU(int n){
+            parent.resize(n+1,1);
+            size.resize(n+1,1);
+            for(int i=0;i<=n;i++){
+                parent[i]=i;
             }
         }
-        return comp-1;
+        int findParent(int n){
+            if(parent[n]==n) return n;
+            return parent[n]=findParent(parent[n]);
+        }
+        void unionBySize(int u,int v){
+            int ulp_u=findParent(u);
+            int ulp_v=findParent(v);
+            if(ulp_u==ulp_v) return ;
+            if(size[ulp_u]>size[ulp_v]){
+                parent[ulp_v]=ulp_u;
+                size[ulp_u]+=size[ulp_v];
+            }
+            else{
+                parent[ulp_u]=ulp_v;
+                size[ulp_v]+=size[ulp_u];
+            }
+        }
+    };
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(n-1>connections.size()) return -1;
+        DSU ds(n);
+        for(int i=0;i<connections.size();i++){
+            ds.unionBySize(connections[i][0],connections[i][1]);
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(ds.findParent(i)==i) c++;
+        }
+        return c-1;
     }
 };
