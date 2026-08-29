3a247fafd06a8924cf60c51f60bd6af6112a55fb
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-08-29 12:00:12 +0530

    Time: 286 ms (31.58%) | Memory: 165.3 MB (18.47%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,51 +1,51 @@
+class DSU{
+    public:
+    vector<int> rank;
+    vector<int> parent;
+    vector<int> size;
+    DSU(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int findParent(int n){
+        if(n==parent[n]) return n;
+        return parent[n]=findParent(parent[n]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_u=findParent(u);
+        int ulp_v=findParent(v);
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u,int v){
+        int ulp_u=findParent(u);
+        int ulp_v=findParent(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
 class Solution {
 public:
-    class DSU{
-        public:
-        vector<int> rank;
-        vector<int> parent;
-        vector<int> size;
-        DSU(int n){
-            rank.resize(n+1,0);
-            parent.resize(n+1,0);
-            size.resize(n+1,1);
-            for(int i=0;i<=n;i++){
-                parent[i]=i;
-            }
-        }
-        int findParent(int n){
-            if(n==parent[n]) return n;
-            return parent[n]=findParent(parent[n]);
-        }
-        void unionByRank(int u,int v){
-            int ulp_u=findParent(u);
-            int ulp_v=findParent(v);
-            if(ulp_u==ulp_v) return;
-            if(rank[ulp_u]<rank[ulp_v]){
-                parent[ulp_u]=ulp_v;
-            }
-            else if(rank[ulp_u]>rank[ulp_v]){
-                parent[ulp_v]=ulp_u;
-            }
-            else{
-                parent[ulp_v]=ulp_u;
-                rank[ulp_u]++;
-            }
-        }
-        void unionBySize(int u,int v){
-            int ulp_u=findParent(u);
-            int ulp_v=findParent(v);
-            if(ulp_u==ulp_v) return;
-            if(size[ulp_u]<size[ulp_v]){
-                parent[ulp_u]=ulp_v;
-                size[ulp_v]+=size[ulp_u];
-            }
-            else{
-                parent[ulp_v]=ulp_u;
-                size[ulp_u]+=size[ulp_v];
-            }
-        }
-    };
     int largestIsland(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
