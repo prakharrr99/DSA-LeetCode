480d632380f0ae3eec00e6e1942b375986737efe
Prakhar Gupta <prakhar.guptapg99@gmail.com>
2026-08-29 22:09:29 +0530

    Time: 164 ms (22.84%) | Memory: 206.2 MB (35.91%) - LeetSync

--- 4366-lexicographically-largest-string-after-pair-transformations/lexicographically-largest-string-after-pair-transformations.cpp
+++ 4366-lexicographically-largest-string-after-pair-transformations/lexicographically-largest-string-after-pair-transformations.cpp
@@ -8,10 +8,14 @@ public:
             while(nums[i]>0){
                 if(nums[i]%2!=0 && a<25) str.push_back(char(a+'a'));
                 if(a>=25){
-                    str.push_back(char('z'));
+                    break;
                 }
                 a++;
                 nums[i]/=2;
+            }
+            while(nums[i]>0){
+                str.push_back('z');
+                nums[i]--;
             }
             reverse(str.begin(),str.end());
             ans.push_back(str);
